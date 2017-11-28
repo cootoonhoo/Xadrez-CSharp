87c0ec543fd2877cf2d552d89f42809a02af78c6
Marco Antonio <cootoonhoo@gmail.com>
2017-11-28 20:57:08 -0200

    Introdução do tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Xadrez
 {
@@ -6,7 +7,12 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow World");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
             Console.ReadLine();
         }
     }
