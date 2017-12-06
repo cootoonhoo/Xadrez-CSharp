4a9f13047387cc9fdcf8f86f6444ed1d337fd7e7
Marco Antonio <cootoonhoo@gmail.com>
2017-12-06 10:40:39 -0200

    Preparando para imprimir as casas possiveis

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -8,16 +8,11 @@ namespace Xadrez
     {
 
         public static void imprimirTabuleiro(Tabuleiro tab) {
+
             for(int i=0; i<tab.linhas; i++){
                 Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++) {
-                    if (tab.peca(i, j) == null) {
-                        Console.Write("- ");
-                    }
-                    else{
                         imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-                    }
                 }
                 Console.WriteLine();
             }
@@ -34,15 +29,25 @@ namespace Xadrez
 
 
         public static void imprimirPeca(Peca peca) {
-            if (peca.cor == Cor.Branca)
+
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write("- ");
             }
-            else {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+            else
+            {
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
     }
